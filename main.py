c3b393e27ea9419d9241138ab867496ff197f14b
Carlos Vitor <limacarlosvitor@gmail.com>
2022-05-30 17:54:30 -0300

    feat: S1-04

--- main.py
+++ main.py
@@ -59,3 +59,119 @@ print(minha_funcao())
 
 print(type(minha_funcao))
 # output: <class 'function'>
+
+string = "Monty Python"
+# Acessando a palavra "Monty"
+string[0:5]
+"Monty"
+# Como queremos desde o inicio da string, podemos omitir o 0
+string[:5]
+"Monty"
+
+# Acessando a palavra "Python"
+string[6:12]
+'Python'
+# Como queremos até o fim da string, podemos omitir o 12,
+# que representa o ultimo indice da string
+string[6:]
+'Python'
+
+string = "Monty Python"
+# Para acessarmos toda a string, podemos fazer o seguinte:
+string[0:12]
+'Monty Python'
+# Ou simplesmente omitir o inicio e fim
+string[:]
+'Monty Python'
+
+# Agora, vamos acessar toda a string e vamos dar um passo 2, vejamos:
+string[0:12:2]
+'MnyPto'
+# omitindo inicio e fim, equivale a:
+string[::2]
+'MnyPto'
+
+# Traz o tamanho de determinados tipos de objeto
+string = "Monty Python"
+len(string)
+12
+
+# Muda o primeiro caractere da string para maiúscula e o restante para minúscula.
+string = "mOnTy pYtHon"
+string.capitalize()
+'Monty python'
+
+# Retorna o número de ocorrências de uma determinada sub-string da string.
+string = "Monty Python"
+string.count("Python")
+1
+string.count('o')
+2
+
+# Retorna uma lista de palavras de uma string, com um separador opcional para ser
+# usado como delimitador. Se nada for passado, como padrão o delimitador será " ".
+string = "Monty Python"
+string.split()
+['Monty', 'Python']
+string = "Monty+Python"
+string.split('+')
+['Monty', 'Python']
+
+# Verifica se a string possui algum conteúdo alfanumérico (letra ou número)
+string = "!@#$%"
+string.isalnum()
+False
+
+# Verifica se a string possui apenas conteúdo alfabético
+string = "Monty Python"
+string.isalpha()
+True
+
+# Verifica se todas as letras de uma string são minúsculas
+string = "Monty Python"
+string.islower()
+False
+
+# Verifica se todas as letras de uma string são maiúsculas
+string = "MONTY PYTHON"
+string.isupper()
+True
+
+# Verifica se a string começa com uma sub-string
+string = "Monty Python"
+string.startswith("Mon")
+True
+
+# Verifica se a string termina com uma sub-string
+string.endswith('hon')
+True
+
+# Substitui uma determinada sub-string por outra
+string = "Monty Python"
+string.replace('Monty', 'Kenzie')
+'Kenzie Python'
+
+# Retorna uma cópia da string trocando todas as letras para minúsculo
+string = "Monty Python"
+string.lower()
+'monty python'
+
+# Ao contrário do lower, o upper troca todas as letras para maiúsculo
+string = "Monty Python"
+string.upper()
+'MONTY PYTHON'
+
+# Esse método inverte o case de cada letra da string (minúsculo / maiúsculo)
+string = "Monty Python"
+string.swapcase()
+'mONTY pYTHON'
+
+# Esse método converte para maiúsculo a primeira letra de cada palavra da string
+string = "monty python"
+string.title()
+'Monty Python'
+
+# Centraliza a string e preenche o espaço restante com o character especificado
+string = 'Kenzie'
+string.center(20, '*')
+'*******Kenzie*******'
