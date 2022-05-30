323b160bb584dc442f55451540a55db336783d9b
Carlos Vitor <limacarlosvitor@gmail.com>
2022-05-30 19:53:32 -0300

    feat: S1-06

--- main.py
+++ main.py
@@ -175,3 +175,165 @@ string.title()
 string = 'Kenzie'
 string.center(20, '*')
 '*******Kenzie*******'
+
+# Podemos criar uma lista com sintaxe literal []:
+minha_lista = [3, "abacate", 9.7, [4, 5, 6], (3, "j")]
+print(minha_lista[3])
+# [4,5,6]
+
+# Utilizando a função list() para trabalhar com lista:
+minha_lista = list("abc")
+print(minha_lista)
+# ['a', 'b', 'c']
+
+minha_lista = list(123)
+print(minha_lista)
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: 'int' object is not iterable
+
+# Para criarmos um valor de 0 a 5:
+minha_lista = list(range(6))
+print(minha_lista)
+# output: [0, 1, 2, 3, 4, 5]
+
+# Para criarmos um valor no intervalo de 15 a 20:
+minha_lista = list(range(15, 21))
+print(minha_lista)
+# output: [15, 16, 17, 18, 19, 20]
+
+# Para criarmos um valor no intervalo de 15 a 20 com um passo de 2:
+minha_lista = list(range(15, 21, 2))
+print(minha_lista)
+# output: [15, 17, 19]
+
+# Primeiramente vamos criar um lista:
+minha_lista = ['1', '2', '3']
+print(minha_lista)
+# output: ['1', '2', '3']
+
+# Vamos alterar o segundo elemento para a palavra 'teste':
+minha_lista[1] = 'teste'
+print(minha_lista)
+# output: ['1', 'teste', '3']
+
+minha_lista = ['1', 'teste', '3']
+
+# Acessando o primeiro elemento:
+print(minha_lista[0])
+# output: 1
+
+# Para acessarmos o último elemento, temos duas maneiras:
+print(minha_lista[2])
+print(minha_lista[-1])
+# output: 3
+
+# Acessando um intervalo do começo da lista até o indice 2
+# Lembrando que o limite (nesse caso indice 2) não é considerado no fatiamento:
+print(minha_lista[0:2])
+# output: ['1', 'teste']
+
+# Acessando o intervalo entre o início e o indice 3 com o passo de 2:
+print(minha_lista[0:3:2])
+# output: ['1', '3']
+
+# Acessando toda lista com o passo de 2:
+print(minha_lista[::2])
+# output: ['1', '3']
+
+minha_lista = [1, 2, 3]
+print(len(minha_lista))
+# output: 3
+
+minha_lista = [1, 2, 3]
+minha_lista.append(4)
+print(minha_lista)
+# output: [1, 2, 3, 4]
+
+minha_lista = [1, 2, 3]
+minha_lista.clear()
+print(minha_lista)
+# output: []
+
+minha_lista = [1, 2, 3]
+lista_copia = minha_lista.copy()
+print(lista_copia)
+# output: [1, 2, 3]
+
+# Repare que o id dos objetos são distintos.
+print(id(minha_lista))
+print(id(lista_copia))
+# output: 140216992200256
+# output: 140216992200192
+
+minha_lista = [1, 2, 3]
+lista_copia = minha_lista
+
+# Repare que o id dos objetos são os mesmos
+print(id(minha_lista))
+print(id(lista_copia))
+# output: 140216992669120
+# output: 140216992669120
+
+# Se fizermos uma alteração na lista cópia, isso se refletirá na primeira lista
+lista_copia[0] = 'teste'
+print(minha_lista)
+print(lista_copia)
+# output: ['teste', 2, 3]
+# output: ['teste', 2, 3]
+
+minha_lista = ["hi", "hello", "olá"]
+print(f'O retorno do método sorted é uma nova listagem: {sorted(minha_lista)}')
+# O retorno do método sorted é uma nova listagem: ['hello', 'hi', 'olá']
+print(f'Percebam que a lista original não foi modificada: {minha_lista}')
+# Percebam que a lista original não foi modificada: ['hi', 'hello', 'olá']
+
+minha_lista = ["hi", "hello", "olá"]
+print(f'O retorno do método sort é {minha_lista.sort()}')
+# O retorno do método sort é None
+print(f'Utilizando sort, a lista original foi modificada = {minha_lista}')
+# Utilizando sort, a lista original foi modificada = ['hello', 'hi', 'olá']
+
+minha_lista = [1, 2, 3]
+print(sorted(minha_lista, reverse=True))
+# [3, 2, 1]
+
+minha_lista = ['melão', 'abacaxi', 'jabuticaba']
+minha_lista.sort(key=len, reverse=True)
+print(minha_lista)
+# ['jabuticaba', 'abacaxi', 'melão']
+
+minha_lista = [1, 2, 3, 4, 1, 2, 1]
+print(minha_lista.count(1))
+# 3
+
+# O método extend() adiciona os itens de um iterável ao final da lista
+minha_lista = [1, 2, 3]
+minha_lista.extend([4, 5, 6])
+print(minha_lista)
+# output: [1, 2, 3, 4, 5, 6]
+
+# Utilizando método append adicionamos o objeto inteiro ao final da lista
+minha_lista = [1, 2, 3]
+minha_lista.append([4, 5, 6])
+print(minha_lista)
+# output:  [1, 2, 3, [4, 5, 6]]
+
+minha_lista = [1, 2, 3]
+print(minha_lista.index(2))
+# 1
+
+minha_lista = [1, 2, 3, 4, 5, 6]
+print(minha_lista.pop())
+# 6
+print(minha_lista)
+# [1, 2, 3, 4, 5]
+print(minha_lista.pop(3))
+# 4
+print(minha_lista)
+# [1, 2, 3, 5]
+
+lista_1 = ['1', '2', '3', '1']
+lista_1.remove('1')
+print(lista_1)
+# ['2', '3', '1']
