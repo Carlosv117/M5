23f2223497540006fb8f19578c8ab4c7826dae74
Carlos Vitor <limacarlosvitor@gmail.com>
2022-06-01 14:23:56 -0300

    feat: S1-14

---
+++ Atividades Kenzie Sprint-1/praticando_parte1/main.py
@@ -0,0 +1,8 @@
+from soma import somar
+from subtracao import subtrair
+
+resultado_da_soma = somar(15, 7)
+print(resultado_da_soma)
+
+resultado_da_subtracao = subtrair(15, 7)
+print(resultado_da_subtracao)
